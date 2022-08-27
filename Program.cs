bd950e0192a1b8659499135f7a2d7e4cf05c6b99
Carlos Decker <carlosdecker022@gmail.com>
2022-08-26 22:29:40 -0300

    Exercício completo

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
+Thread.Sleep(1000);
+Console.Write("Pressione uma tecla para continuar");
+Console.ReadKey();
